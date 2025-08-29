8860d3a1966a82597ddd71f0fa80832580e1b20b
juyoung07 <yunju5090@gmail.com>
2025-08-29 14:45:55 +0900

    #2 결과 내역, 변수 선언, 결과 데이터 파일을 읽어오는

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace MyTarot
 {
     public partial class Form1 : Form
     {
+
+        List<string> results;
+
         public Form1()
         {
             InitializeComponent();
+            LoadAdvice();
         }
+
+        private void LoadAdvice()
+        {
+            try
+            {
+                string filename = "results.csv";
+                results = File.ReadAllLines(filename).ToList();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"파일이 없음. \n{ex.Message}", "파일이 없는 오류!");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"권한이 없음. \n{ex.Message}", "파일 권한 오류!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류가 발생함. \n{ex.Message}", "알 수 없는 오류!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
 
         private void label2_Click(object sender, EventArgs e)
         {
@@ -52,7 +81,16 @@ namespace MyTarot
 
         private void 결과불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
+            FormHistory form = Application.OpenForms["FormHistory"] as FormHistory;
+            if (form != null)
+            {
+                form.Activate();
+            }
+            else
+            {
+                form = new FormHistory(this);
+                form.Show();
+            }
         }
     }
 }
